72be99ca18b80ce7a1eb5cac003c6058ad446956
pharrukh <farrukh.normuradov@gmail.com>
2021-01-14 16:24:33 +0100

    chore: clean-up for the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { SuccessAlert } from './success-alert/success-alert-component';
-import { WarningAlert } from './warning-alert/warning-alert.component';
 import { FormsModule } from '@angular/forms';
-import { BindingComponent } from './binding/binding.component';
-import { DirectiveComponent } from './directive/directive.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    SuccessAlert,
-    WarningAlert,
-    BindingComponent,
-    DirectiveComponent,
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/warning-alert/warning-alert.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'warning-alert',
-  template: '<p>WARNING!</p>',
-  styleUrls: ['warning-alert.component.css'],
-})
-export class WarningAlert {}
